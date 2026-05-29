a0e33aabf98edefe64c6220ea8bf99f0a91e6427
NAMELESS-ADROIT <justinemwala0069@gmail.com>
2026-05-29 14:37:34 +0300

    feat/ added a fully functioning dark-mode toggle switch

--- scripts/profile6-justine.js
+++ scripts/profile6-justine.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+//light-dark toggle switch
+const switchToggle = document.querySelector('input[type=checkbox]');
+
+//once checked, this function tuns and the dark mode is triggered in the css
+switchToggle.addEventListener('change', ()=>{
+    document.body.classList.toggle('dark-mode', switchToggle.checked);
+});
