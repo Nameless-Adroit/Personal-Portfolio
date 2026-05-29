553f3ee9800e8f6dc90c0739e901c53d9e6f0b4c
NAMELESS-ADROIT <justinemwala0069@gmail.com>
2026-05-29 17:37:51 +0300

    feat/ added a dropdown effect on skills section

--- scripts/profile6-justine.js
+++ scripts/profile6-justine.js
@@ -5,3 +5,22 @@ const switchToggle = document.querySelector('input[type=checkbox]');
 switchToggle.addEventListener('change', ()=>{
     document.body.classList.toggle('dark-mode', switchToggle.checked);
 });
+
+// clicking it expands a hidden details paragraph
+document.querySelectorAll('.skill-tag').forEach(button => {
+    button.addEventListener('click', () => {
+        const desc = button.nextElementSibling;
+
+        // Close all others that have not been clicked.
+        document.querySelectorAll('.skill-tag').forEach(other => {
+            if (other !== button) {
+                other.nextElementSibling.classList.remove('open');
+                other.classList.remove('open');
+            }
+        });
+
+        // Toggle current one that has been clicked
+        desc.classList.toggle('open');
+        button.classList.toggle('open');
+    });
+});
